0c52885138d0be8e2f6a285bcf2018533512a344
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-12 14:32:05 -0500

    console add 2 task

--- console.py
+++ console.py
@@ -19,16 +19,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
     classes = {
-               'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'State': State, 'City': City, 'Amenity': Amenity,
-               'Review': Review
-              }
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
     types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,15 +113,50 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    @staticmethod
+    def args_manager(args):
+        """Static method that review the args
+        from create to update the instance
+        Args:
+            args ([str]): Args input to created method
+
+        Returns:
+            [str, dic]: Classname and dictionary with attributes
+        """
+        args = args.split()
+        temp_class = args.pop(0)
+        args_dic = {}
+        for arg in args:
+            key, value = arg.split("=")
+            if value[0] == '"' and value[-1] == '"':
+                aux_value = value[1:-1]
+                for index in range(len(aux_value)):
+                    if aux_value[index] == '"' and aux_value[index - 1] != "\\":
+                        aux_value.replace('"', '\\"')
+                    elif aux_value[index] == '_':
+                        aux_value.replace('_', ' ')
+                args_dic[key] = aux_value
+            else:
+                try:
+                    args_dic[key] = int(value)
+                except:
+                    try:
+                        args_dic[key] = float(value)
+                    except:
+                        pass
+        return temp_class, args_dic
+
     def do_create(self, args):
         """ Create an object of any class"""
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        args, args_dic = self.args_manager(args)
+        if args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
         new_instance = HBNBCommand.classes[args]()
+        new_instance.__dict__.update(args_dic)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -319,6 +354,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
