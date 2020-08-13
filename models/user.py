5505a861bcc10295c911298dfcb46f5251ff34dd
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-13 12:21:39 -0500

    task 7. DBStorage - User and task 8. DBStorage - Place, Update user class, place class and city class

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
 
 Base = declarative_base()
 
@@ -12,5 +13,11 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place',
+                          backref='user',
+                          cascade='all, delete-orphan')
+    reviews = relationship('Review',
+                           backref='user',
+                           cascade='all, delete-orphan')
