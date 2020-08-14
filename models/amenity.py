dbf0d53b23f904edc1923b327ef5e20b501e9711
Andres Felipe Escobar <felipeescobar15@gmail.com>
2020-08-14 00:28:56 +0000

    Fixed issues in the file amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
     """ Amenity class """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   back_populates='amenities')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates='amenities')
