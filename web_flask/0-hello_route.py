331d8f2de1fd217097127780bda59041b486dbed
monicajoa <mnortiz.ortiz@gmail.com>
2020-09-02 21:21:30 -0500

    AirBnB clone - Web framework project, mandatory task 0, hello flask, add two files 0-hello_route.py, __init__.py script that starts a Flask web application, add documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Hello Flask
     This module holds a script that starts a Flask web application
+    web application must be listening on 0.0.0.0, port 5000
+    Routes: /: display “Hello HBNB!”
+    using the option strict_slashes=False in your route definition
 """
 from flask import Flask
 app = Flask(__name__)
