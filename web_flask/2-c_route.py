8ce49ac8be8d40d0864db7cfc205af58e554eb27
monicajoa <mnortiz.ortiz@gmail.com>
2020-09-02 22:14:14 -0500

    AirBnB clone - Web framework project, mandatory task 2. C is fun, add 2-c_route.py and file task one, script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" HBNB
+""" C is fun
     This module holds a script that starts a Flask web application
     web application must be listening on 0.0.0.0, port 5000
     Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
-    using the option strict_slashes=False in the route definition
+    /c/<text> display “C ” followed by the value of the text variable
+    using the option strict_slashes=False in your route definition
 """
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -31,5 +33,18 @@ def hbnb():
     return "HBNB"
 
 
-if __name__ == "__main__":
-    app.run('0.0.0.0', 5000)
+@app.route('/c/<text>', strict_slashes=False)
+def display_text(text):
+    """ Method that returns “C <text> from a client requests
+
+    Args:
+        text ([str]): variable in the url
+
+    Returns:
+        [str]: Display “C ” followed by the value of the text variable
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
